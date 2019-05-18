52a7547e6a8f2c3690c5f06d8daec09386de0b91
Jayden Chin <weihong118118@gmail.com>
2019-05-18 20:51:16 +0900

    Upload SOM & Hybrid model (SOM+ANN) code

---
+++ Week5-SOM/SOM.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 17 12:36:48 2019
+
+@author: chinweihong
+"""
+
+# Self Organizing Map
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Credit_Card_Applications.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (0,1))
+X = sc.fit_transform(X)
+
+# Training the SOM
+from minisom import MiniSom
+som = MiniSom(x=15, y=15, input_len=15, sigma=1.0, learning_rate=0.5)
+som.random_weights_init(X)
+som.train_random(data=X, num_iteration=100)
+
+# Visualizing the results
+from pylab import bone, pcolor, colorbar, plot, show
+bone()
+pcolor(som.distance_map().T)
+colorbar()
+markers = ['o', 's']
+colors = ['r', 'g']
+for i, x in enumerate(X):
+    w = som.winner(x)
+    plot(w[0] + 0.5,
+         w[1] + 0.5,
+         markers[y[i]],
+         markeredgecolor=colors[y[i]],
+         markerfacecolor='None',
+         markersize=10,
+         markeredgewidth=2)
+show()
+
+# Finding the frauds
+mappings = som.win_map(X)
+frauds = np.concatenate((mappings[(10,8)], mappings[(9,10)]), axis=0)
+frauds = sc.inverse_transform(frauds)
